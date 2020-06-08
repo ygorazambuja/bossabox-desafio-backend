07cb7ff120bf4464915a0aca1d46c62c16f2989d
Ygor Azambuja <ygorazambuja@gmail.com>
2020-06-08 10:40:58 -0400

    chore: put app start again

--- src/server.ts
+++ src/server.ts
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/strict-boolean-expressions */
 import App from './app'
 import ToolController from './controllers/tool.controller'
 import UserController from './controllers/user.controller'
@@ -5,8 +6,9 @@ import AuthController from './controllers/auth.controller'
 
 const app = new App(
   [ToolController, UserController, AuthController],
-  // eslint-disable-next-line @typescript-eslint/strict-boolean-expressions
   process.env.PORT || 3000
 )
+
+app.start()
 
 export default app
